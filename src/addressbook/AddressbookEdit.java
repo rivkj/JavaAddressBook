ba94a2b0a1108ca4b38f3bde3567730741b15a49
Kimberly Rivas <rivaskj07@gmail.com>
2022-08-03 21:20:07 -0400

    Edited address book to remove and quit program

---
+++ src/addressbook/AddressbookEdit.java
@@ -0,0 +1,112 @@
+package addressbook;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AddressbookEdit {
+
+  public static Scanner sc;
+
+  public static void main(String[] args) {
+    sc = new Scanner(System.in);
+    int input = 0;
+
+    Addressbook addressbook = new Addressbook();
+    while (input != 6) {
+      printMenu();
+      input = getIntInput();
+      if (input < 1 || input > 6) {
+        System.out.println("Invalid input! Try Again");
+      } else {
+        switch (input) {
+          case 1:
+            addEntry(addressbook);
+            break;
+          case 2:
+            String email = askForInput("email");
+            addressbook.removeEntry(email);
+            break;
+          case 3:
+            searchForSpecificEntry(addressbook);
+            break;
+          case 4:
+            addressbook.printAddressBook();
+            break;
+          case 5:
+            addressbook.deleteAddressBook();
+            break;
+          default:
+            System.out.println("Exiting program");
+        }
+      }
+    }
+
+    sc.close();
+  }
+
+  public static int getIntInput() {
+    String ip = sc.nextLine();
+    while (ip.length() == 0) {
+      ip = sc.nextLine();
+    }
+    return Integer.parseInt(ip);
+  }
+
+  public static String askForInput(String inputName) {
+    System.out.println("Please enter " + inputName + ": ");
+    return sc.nextLine();
+  }
+
+  public static void addEntry(Addressbook addressBook) {
+    System.out.print("Enter first name: ");
+    String firstName = sc.nextLine();
+
+    System.out.print("Enter last name: ");
+    String lastName = sc.nextLine();
+
+    System.out.print("Enter phone number: ");
+    String phone = sc.nextLine();
+
+    System.out.print("Enter email: ");
+    String email = sc.next().strip();
+
+    Entry entry = new Entry(firstName, lastName, phone, email);
+    addressBook.addEntry(entry);
+  }
+
+  public static void printMenu() {
+    System.out.println("Please select an option:");
+    System.out.println("1. Add an entry");
+    System.out.println("2. Remove an entry");
+    System.out.println("3. Search for an specific entry");
+    System.out.println("4. Print the contents of the address book");
+    System.out.println("5. Delete the contents of the address book");
+    System.out.println("6. Quit the program");
+  }
+
+  public static void searchForSpecificEntry(Addressbook addressBook) {
+    System.out.println("Please select an option");
+    System.out.println("1. Search By First Name");
+    System.out.println("2. Search By Last Name");
+    System.out.println("3. Search By Phone");
+    System.out.println("4. Search By Email");
+
+    int input = getIntInput();
+
+    Map<Integer, String> searchMapping =
+        Map.of(1, "firstName", 2, "lastName", 3, "phone", 4, "email");
+    if (input < 0 || input > 4) {
+      System.out.println("Invalid input! Try again");
+    } else {
+      System.out.print("Enter your search query: ");
+      String searchQuery = sc.nextLine();
+      List<Entry> entries = addressBook.searchForAnEntry(searchMapping.get(input), searchQuery);
+      System.out.println("Here are your search results:");
+      for (Entry entry : entries) {
+        System.out.println(entry);
+      }
+    }
+    System.out.println();
+  }
+}
---
+++ src/addressbook/Entry.java
@@ -0,0 +1,84 @@
+package addressbook;
+
+import java.util.Objects;
+
+public class Entry {
+  private String firstName;
+  private String lastName;
+  private String phone;
+  private String email;
+
+  public Entry(String firstName, String lastName, String phone, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Entry entry = (Entry) o;
+    return Objects.equals(getEmail(), entry.getEmail());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getEmail());
+  }
+
+  @Override
+  public String toString() {
+    return "Entry{"
+        + "firstName='"
+        + firstName
+        + '\''
+        + ", lastName='"
+        + lastName
+        + '\''
+        + ", phone='"
+        + phone
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + '}';
+  }
+}
